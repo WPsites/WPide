cb45f18208cd4910b272916c48cf98cfc10a7e60
Simon Dunton <simon@wpsites.co.uk>
2012-02-19 21:48:58 +0000

    Initial commit of V2 development branch

--- WPide.php
+++ WPide.php
@@ -3,8 +3,8 @@
 
 Plugin Name: WPide
 Plugin URI: https://github.com/WPsites/WPide
-Description: Replace the default WordPress code editor for plugins and themes. Adding syntax highlighting, autocomplete of WordPress functions + PHP, line numbers, auto backup of files before editing.
-Version: 1.0.2
+Description: WordPress code editor for plugins and themes. Adding syntax highlighting, autocomplete of WordPress functions + PHP, line numbers, auto backup of files before editing, tabbed editor.
+Version: 2.0
 Author: Simon Dunton
 Author URI: http://www.wpsites.co.uk
 
@@ -16,33 +16,142 @@ class WPide
 
 {
 
-
+	public $site_url, $plugin_url;
+	
 	function __construct() {
-
-		// Uncomment any of these calls to add the functionality that you need.
-		add_action('admin_head', 'WPide::add_admin_head');
-		add_action('admin_head', 'WPide::load_editor');
-		add_action('admin_init', 'WPide::add_admin_js');
-
-		//setup ajax function to save a backup
-		add_action('wp_ajax_ace_backup_call', 'WPide::ace_backup_call');
+	
+		//add WPide to the menu
+		add_action( 'admin_menu',  array( &$this, 'add_my_menu_page' ) );
+		
+		//only include this plugin if on theme editor, plugin editor or an ajax call
+		if ( $_SERVER['PHP_SELF'] === '/wp-admin/plugin-editor.php' || 
+				$_SERVER['PHP_SELF'] === '/wp-admin/theme-editor.php' ||
+					$_SERVER['PHP_SELF'] === '/wp-admin/admin-ajax.php' ||
+					$_GET['page'] === 'wpide' ){
+		
+					// Uncomment any of these calls to add the functionality that you need.
+					add_action('admin_head', 'WPide::add_admin_head');
+					add_action('admin_head', 'WPide::load_editor');
+					add_action('admin_init', 'WPide::add_admin_js');
+					//add_action('admin_head', 'WPide::add_admin_styles');
+					
+					//setup ajax function to save a backup
+					add_action('wp_ajax_ace_backup_call', 'WPide::ace_backup_call');
+					//setup ajax function to get file contents for editing 
+					add_action('wp_ajax_wpide_get_file', 'WPide::wpide_get_file' );
+					//setup ajax function to save file contents
+					add_action('wp_ajax_wpide_save_file', 'WPide::wpide_save_file' );
+		
+		}
+		
+		$WPide->site_url = get_bloginfo('url');
+		
 
 	}
+
 
 
     public static function add_admin_head()
     {
 
     ?>
-
+	<link rel='stylesheet' href='<?php echo plugins_url("jqueryFileTree.css", __FILE__ );?>' type='text/css' media='all' />
+		
       <style type="text/css">
 	#quicktags, #post-status-info, #editor-toolbar, #newcontent, .ace_print_margin { display: none; }
     #fancyeditordiv {
 	  position: relative;
-	  width: 500px;
+	  width: 70%;
 	  height: 400px;
 	}
 	#template div{margin-right:0 !important;}
+	
+	#wpide_toolbar{
+		width:75%;
+		height:30px;
+		text-align:right;
+		padding-top:20px;
+		position:relative;
+	}
+	#wpide_toolbar form{
+		position: absolute;
+		left: 104%;
+	}
+	#wpide_toolbar_tabs{
+		width:75%;
+	}
+	
+	#wpide_toolbar .wpide_tab {
+		height: 18px;
+		font: 13px/18px Arial,Helvetica,sans-serif normal;
+		margin-top: -2px;
+		margin-right: 2px;
+		padding: 6px;
+		float: left;
+		cursor: pointer;
+		border-width: 1px;
+		border-style: solid;
+		-webkit-border-top-right-radius: 3px;
+		-webkit-border-top-left-radius: 3px;
+		border-top-right-radius: 3px;
+		border-top-left-radius: 3px;
+		background-color: #E8E8E8;
+		border-color: #DFDFDF #DFDFDF #CCC;
+		color: #21759B;
+		text-decoration: none;
+		font-style: italic;
+	}
+	
+	#wpide_file_browser{
+		margin-right:2%;
+		text-align:left;
+	}
+	
+	.toplevel_page_wpide #submitdiv{
+		 width:22%;
+		 float:right;
+	 }
+	 .toplevel_page_wpide #submitdiv h3.hndle{
+	 	font-family: Georgia,"Times New Roman";
+		font-size: 15px;
+		font-weight: bold;
+		padding: 7px 10px;
+		margin: 0;
+		line-height: 1;
+	 }
+	 
+	 #wpide_toolbar_buttons {
+	 	position:relative;
+	 	min-height:30px;
+		width:75%;
+		float:left;
+		text-align:left;
+		overflow:hidden;
+		border-bottom: 1px solid #CCC;
+		background-color: #E9E9E9;
+		background-image: -ms-linear-gradient(bottom,#ddd,#e9e9e9);
+		background-image: -moz-linear-gradient(bottom,#ddd,#e9e9e9);
+		background-image: -o-linear-gradient(bottom,#ddd,#e9e9e9);
+		background-image: -webkit-linear-gradient(bottom,#ddd,#e9e9e9);
+		background-image: linear-gradient(bottom,#ddd,#e9e9e9);
+		
+		-webkit-border-top-right-radius: 6px;
+		-webkit-border-top-left-radius: 0px;
+		border-top-right-radius: 6px;
+		border-top-left-radius: 0px;
+		}
+		
+		#wpide_toolbar_buttons a{
+			display:block;
+			float:left;
+			margin:6px;
+		}
+		
+		#wpwrap div.ace_gutter{
+			background-color:#f4f4f4;
+			color:#aaa;
+		}
+	
     </style>
 
     <?php
@@ -1451,6 +1560,73 @@ var html_tags = ("_() __() __checked_selected_helper() __construct() __destruct(
 "zend_version() ").split(" ");
 
 
+var saved_editor_sessions = [];
+var last_added_editor_session = 0;
+var current_editor_session = 0;
+
+	
+			
+
+var EditSession = require('ace/edit_session').EditSession; 
+//add the file contents to our new editor instance
+
+
+
+//are we editing a theme or plugin?
+<?php if($_SERVER['PHP_SELF'] === '/wp-admin/plugin-editor.php'){ ?>
+  var aceedittype = 'plugin';
+<?php }elseif($_SERVER['PHP_SELF'] === '/wp-admin/theme-editor.php'){?>
+  var aceedittype = 'theme';
+<?php }else{?>
+  var aceedittype = 'theme';
+<?php }//end ?>
+
+
+//open another file and add to editor
+function wpide_set_file_contents(file){
+	//ajax call to get file contents we are about to edit
+	var data = { action: 'wpide_get_file', filename: file };
+			jQuery.post(ajaxurl, data, function(response) { 
+			
+			//editor.sessions.push
+			//saved_editor_sessions.push( file );
+			the_path = file.replace(/^.*[\\\/]/, ''); 
+			jQuery("#wpide_toolbar_tabs").append('<a href="#" id="wpide_tab_'+last_added_editor_session+'" sessionrel="'+last_added_editor_session+'"  title="  '+file+' " rel="'+file+'" class="wpide_tab">'+ the_path +'</a>');
+	
+			editor.gotoLine(1);
+			editor.getSession().setValue( response );
+			editor.resize(); 
+			editor.focus(); 		
+			
+			saved_editor_sessions[last_added_editor_session] = new EditSession(response);//set saved session
+			last_added_editor_session++; //increment session counter
+			
+			
+				jQuery(".wpide_tab").off('click').on("click", function(event){
+					event.preventDefault();
+					console.log( jQuery(this).attr('rel') + " opened" );
+					jQuery('input[name=filename]').val( jQuery(this).attr('rel') );
+					//save current editor into session
+					//saved_editor_sessions[current_editor_session] = editor.getSession(); 
+					//get old editor out of session and apply to editor
+					clicksesh = jQuery(this).attr('sessionrel');
+					editor.setSession( saved_editor_sessions[ clicksesh ] );
+					
+					//use editors php mode
+					var phpMode = require("ace/mode/php").Mode;
+					editor.getSession().setMode(new phpMode());
+					
+					editor.resize(); 
+					editor.focus(); 
+					//make a not of current editor
+					current_editor_session = clicksesh;
+					
+				});
+			
+				return response;
+			
+	});
+}
 
 
 
@@ -1458,30 +1634,33 @@ var html_tags = ("_() __() __checked_selected_helper() __construct() __destruct(
 
 		jQuery(document).ready(function($) {
 
-			//quit if editing this plugin since it will spaz out!!
-			if ( $('input[name=file]').val() == 'ace/ace.php' ){
-				$('#newcontent').css({'display': 'inline', 'width': '70%'}); //unhide the usual textarea
-				return;
-			}
+			$("#wpide_save").click(function(){
+						//ajax call to generate a backup of this file we are about to edit
+						var data = { action: 'wpide_save_file', filename: $('input[name=filename]').val(), content: editor.getSession().getValue() };
+								jQuery.post(ajaxurl, data, function(response) { 
+								if (response === 'success'){
+									$("#wpide_message").html('<span>File saved.</span>');
+									$("#wpide_message").show();
+									$("#wpide_message").fadeOut(5000); 
+								}else{
+									alert(response);
+								}
+						});
+			});
 			
+			
+	
 			//add div for ace editor to latch on to
-			$('#template').prepend("<div style='width:80%;height:500px;margin-right:0!important;' id='fancyeditordiv'></div>");
+			//$('#template').prepend("<div style='width:80%;height:500px;margin-right:0!important;' id='fancyeditordiv'></div>");
 			//create the editor instance
-  			editor = ace.edit("fancyeditordiv");
+			editor = ace.edit("fancyeditordiv");
 			//set the editor theme
 			editor.setTheme("ace/theme/dawn"); 
 			//get a copy of the initial file contents (the file being edited)
-			var intialData = $('#newcontent').val()
-			//add the file contents to our new editor instance
+			//var intialData = $('#newcontent').val()
+			var intialData = "Use the file manager to find a file you wish edit, click the file name to edit. \n\n";
 			editor.getSession().setValue( intialData );
-
-
-			//are we editing a theme or plugin?
-			<?php if($_SERVER['PHP_SELF'] === '/wp-admin/plugin-editor.php'){ ?>
-			  var aceedittype = 'plugin';
-			<?php }elseif($_SERVER['PHP_SELF'] === '/wp-admin/theme-editor.php'){?>
-			  var aceedittype = 'theme';
-			<?php }//end ?>
+		
 
 
 			//ajax call to generate a backup of this file we are about to edit
@@ -1774,20 +1953,22 @@ var html_tags = ("_() __() __checked_selected_helper() __construct() __destruct(
 
     public static function add_admin_js()
     {
-        $plugin_path =  get_bloginfo('url').'/wp-content/plugins/' . basename(dirname(__FILE__)) .'/';
+        
+		//include file tree
+		wp_enqueue_script('jquery-file-tree', plugins_url("jqueryFileTree.js", __FILE__ ) );
 		//include ace
-        wp_enqueue_script('ace', $plugin_path . 'ace-0.2.0/src/ace.js');
+        	wp_enqueue_script('ace', plugins_url("ace-0.2.0/src/ace.js", __FILE__ ) );
 		//include ace mode
-        wp_enqueue_script('ace-mode', $plugin_path . 'ace-0.2.0/src/mode-php.js');
+        	wp_enqueue_script('ace-mode', plugins_url("ace-0.2.0/src/mode-php.js", __FILE__ ) );
 		//include ace theme
-		wp_enqueue_script('ace-theme', $plugin_path . 'ace-0.2.0/src/theme-dawn.js');//monokai is nice
+		wp_enqueue_script('ace-theme', plugins_url("ace-0.2.0/src/theme-dawn.js", __FILE__ ) );//monokai is nice
     }
 
 
 	public static function ace_backup_call() {
 
-		$backup_path =  get_bloginfo('url').'/wp-content/plugins/' . basename(dirname(__FILE__)) .'/backups/';
-		$file_name = $_POST['filename'];
+		$backup_path =  $WPide->site_url .'/wp-content/plugins/' . basename(dirname(__FILE__)) .'/backups/';
+		$file_name = stripslashes($_POST['filename']);
 		$edit_type = $_POST['edittype'];
 
 		if ($edit_type==='theme'){
@@ -1809,6 +1990,8 @@ var html_tags = ("_() __() __checked_selected_helper() __construct() __destruct(
 				}else if(!file_exists($new_file_path_hourly)){
                     $backup_result = copy($file_name, $new_file_path_hourly); //make a copy of the file
 				}
+				
+				
 
 				//do no further backups since one intial backup for today and an hourly one is plenty!
 
@@ -1846,15 +2029,95 @@ var html_tags = ("_() __() __checked_selected_helper() __construct() __destruct(
 		die(); // this is required to return a proper result
 
 	}
+	
+	public static function wpide_get_file() {
+
+		$file_name = stripslashes($_POST['filename']);
+		echo file_get_contents($file_name);
+		die(); // this is required to return a proper result
+	}
+	
+	public static function wpide_save_file() {
+
+		$file_name = stripslashes($_POST['filename']);
+		
+		//set backup filename
+		$backup_path =  ABSPATH .'wp-content/plugins/' . basename(dirname(__FILE__)) .'/backups/' . str_replace( str_replace('\\', "/", ABSPATH), '', $file_name) .'.'.date("YmdH");
+		//create backup directory if not there
+		$new_file_info = pathinfo($backup_path);
+		if (!is_dir($new_file_info['dirname'])) mkdir($new_file_info['dirname'], 0777, true); //make directory if not exist
+		//do backup
+		file_put_contents($backup_path,  file_get_contents($file_name) );
+		
+		if( file_put_contents($file_name, stripslashes($_POST['content'])  ) ) echo "success";
+		die(); // this is required to return a proper result
+	}
+	
+	public function add_my_menu_page() {
+		//add_menu_page("wpide", "wpide","edit_themes", "wpidesettings", array( &$this, 'my_menu_page') );
+		add_menu_page('wpide', 'wpide', 'edit_themes', "wpide", array( &$this, 'my_menu_page' ));
+	}
+	
+	public function my_menu_page() {
+		if ( !current_user_can('edit_themes') )
+		wp_die('<p>'.__('You do not have sufficient permissions to edit templates for this site. SORRY').'</p>');
+		
+		?>
+		<script>
+		jQuery(document).ready( function($) {
+			$('#wpide_file_browser').fileTree({ root: '<?php echo str_replace('\\', "/", WP_CONTENT_DIR);?>/', script: '<?php echo plugins_url("jqueryFileTree.php", __FILE__ );?>' }, function(file) {
+				wpide_set_file_contents(file);
+				$('#filename').val(file);
+			});
+		});
+		</script>
+		
+		
+			
+<div id="wpide_toolbar" class="quicktags-toolbar"> 
+  <div id="wpide_toolbar_tabs"> </div>
+  <form action="" method="get">
+    <a href="#" id="wpide_save" class="button-primary" style="margin-right:25px;">SAVE 
+    FILE</a> 
+    <input type="hidden" id="filename" name="filename" value="" />
+  </form>
+</div>
+			
+<div id="wpide_toolbar_buttons"> 
+  <div id="wpide_message" class="error highlight" style="display:none;width: 97%;position: absolute;top: 1px;left: 2px;text-align:left;margin:0;padding:5px;"></div>
+  <a href="#"></a> <a href="#"></a> </div>
+			
+			
+<div style='width:75%;height:650px;margin-right:0!important;float:left;' id='fancyeditordiv'></div>	
+			
+			
+			<div id="submitdiv" class="postbox "> 
+			  <div class="handlediv" title="Click to toggle"><br>
+			  </div>
+			  <h3 class="hndle"><span>Files</span></h3>
+			  <div class="inside"> 
+				<div class="submitbox" id="submitpost"> 
+				  <div id="minor-publishing"> 
+				  </div>
+				  <div id="major-publishing-actions"> 
+					<div id="wpide_file_browser"></div>
+					<br style="clear:both;" />
+					<div id="publishing-action"> <img src="/wp-admin/images/wpspin_light.gif" class="ajax-loading" id="ajax-loading" alt="" style="visibility: hidden; "> 
+					  <input name="save" type="submit" class="button-primary" id="publish" tabindex="5" accesskey="p" value="Update">
+					</div>
+					<div class="clear"></div>
+				  </div>
+				</div>
+			  </div>
+			</div>
+
+
+			
+		<?php
+	}
 
 }
 
-//only include this plugin if on theme editor, plugin editor or an ajax call
-if ( $_SERVER['PHP_SELF'] === '/wp-admin/plugin-editor.php' || 
-		$_SERVER['PHP_SELF'] === '/wp-admin/theme-editor.php' ||
-			$_SERVER['PHP_SELF'] === '/wp-admin/admin-ajax.php' ){
 
-	add_action("init", create_function('', 'new WPide();'));
-
-}
+add_action("init", create_function('', 'new WPide();'));
 ?>

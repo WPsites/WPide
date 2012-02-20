d8d352c1247546dbd939d7a9551069c456ba8a8c
Thomas Wieczorek <wieczo.yo@googlemail.com>
2012-02-21 00:35:59 +0100

    moved autocomplete to a separate function

--- js/load-editor.js
+++ js/load-editor.js
@@ -22,7 +22,140 @@ else {
 	aceedittype = 'theme';
 }	
 
+function onSessionChange(e)  {
+	//don't continue with autocomplete if /n entered
+	try {
+		if ( e.data.text.charCodeAt(0) === 10 ){
+			return;
+		}
+	}catch(e){}
 
+	//get cursor/selection
+	var range = editor.getSelectionRange();
+	
+	//do we need to extend the length of the autocomplete string
+	if (autocompleting) {
+		autocompletelength = autocompletelength + 1;
+	} else {
+		autocompletelength = 2;
+	}
+
+	//modify the cursor/selection data we have to get text from the editor to check for matching function/method
+	//set start column
+	range.start.column = range.start.column - autocompletelength;
+	//no column lower than 1 thanks
+	if (range.start.column < 1) {
+		range.start.column = 0;
+	}
+	//set end column
+	range.end.column = range.end.column + 1;
+	//get the editor text based on that range
+	var text = editor.getSession().doc.getTextRange(range);
+
+	//dont show if no text passed
+	$quit_onchange = false;
+	try {
+		if (text==="") {
+			ac.style.display='none';
+		}
+	} catch(e) { }//catch end
+	// if string length less than 3 then quit this
+	if (text.length < 3){
+		return;
+	}
+
+
+	//create the dropdown for autocomplete
+	var sel = editor.getSelection();
+	var session = editor.getSession();
+	var lead = sel.getSelectionLead();
+
+	var pos = editor.renderer.textToScreenCoordinates(lead.row, lead.column);
+	var ac;
+
+
+
+	if( document.getElementById('ac') ){
+		ac=document.getElementById('ac');
+
+		//editor clicks should hide the autocomplete dropdown
+		editor.container.addEventListener('click',function(e){
+			ac.style.display='none';
+		});
+
+	} //end - create initial autocomplete dropdown and related actions
+
+
+	//calulate the editor container offset
+	var obj=editor.container;
+
+	var curleft = 0;
+	var curtop = 0;
+
+	if (obj.offsetParent) {
+
+		do {
+			curleft += obj.offsetLeft;
+			curtop += obj.offsetTop;
+		} while (obj = obj.offsetParent);
+
+	}						
+
+
+	//position autocomplete
+	ac.style.top=pos.pageY - curtop + 40 + "px";
+	ac.style.left=pos.pageX - curleft + 20 + "px";
+	ac.style.display='block';
+	ac.style.background='white';
+
+
+	//remove all options, starting a fresh list
+	ac.options.length = 0;
+
+	//loop through tags and check for a match
+	var tag;
+	for(i in html_tags) {
+		if(!html_tags.hasOwnProperty(i) ){
+			continue;
+		}
+
+		tag=html_tags[i];					
+		if( text ){
+			if( text !== tag.substr(0,text.length) ){
+				continue;
+			}
+		}
+
+		var option = document.createElement('option');
+		option.text = tag;
+		option.value = tag;
+
+		try {
+			ac.add(option, null); // standards compliant; doesn't work in IE
+		}
+		catch(ex) {
+			ac.add(option); // IE only
+		}
+
+	}//end for
+
+
+
+	//if the return list contains everything then don't display it
+	if (html_tags.length === ac.options.length){
+		ac.options.length = 0;
+	}
+
+	//check for matches
+	if ( ac.length === 0 ) {
+		ac.style.display='none';
+		autocompleting=false;
+	} else {
+		ac.selectedIndex=0;			
+		autocompleting=true;
+	}
+
+}
 
 //open another file and add to editor
 function wpide_set_file_contents(file){
@@ -140,140 +273,7 @@ jQuery(document).ready(function($) {
 	editor.container.appendChild(ac);
 
 	//hook onto any change in editor contents
-	editor.getSession().on('change', function(e) {
-		//don't continue with autocomplete if /n entered
-		try {
-			if ( e.data.text.charCodeAt(0) === 10 ){
-				return;
-			}
-		}catch(e){}
-	
-		//get cursor/selection
-		var range = editor.getSelectionRange();
-		
-		//do we need to extend the length of the autocomplete string
-		if (autocompleting) {
-			autocompletelength = autocompletelength + 1;
-		} else {
-			autocompletelength = 2;
-		}
-
-		//modify the cursor/selection data we have to get text from the editor to check for matching function/method
-		//set start column
-		range.start.column = range.start.column - autocompletelength;
-		//no column lower than 1 thanks
-		if (range.start.column < 1) {
-			range.start.column = 0;
-		}
-		//set end column
-		range.end.column = range.end.column + 1;
-		//get the editor text based on that range
-		var text = editor.getSession().doc.getTextRange(range);
-	
-		//dont show if no text passed
-		$quit_onchange = false;
-		try {
-			if (text==="") {
-				ac.style.display='none';
-			}
-		} catch(e) { }//catch end
-		// if string length less than 3 then quit this
-		if (text.length < 3){
-			return;
-		}
-
-
-		//create the dropdown for autocomplete
-		var sel = editor.getSelection();
-		var session = editor.getSession();
-		var lead = sel.getSelectionLead();
-
-		var pos = editor.renderer.textToScreenCoordinates(lead.row, lead.column);
-		var ac;
-
-
-
-		if( document.getElementById('ac') ){
-			ac=document.getElementById('ac');
-
-			//editor clicks should hide the autocomplete dropdown
-			editor.container.addEventListener('click',function(e){
-				ac.style.display='none';
-			});
-
-		} //end - create initial autocomplete dropdown and related actions
-
-
-		//calulate the editor container offset
-		var obj=editor.container;
-
-		var curleft = 0;
-		var curtop = 0;
-
-		if (obj.offsetParent) {
-
-			do {
-				curleft += obj.offsetLeft;
-				curtop += obj.offsetTop;
-			} while (obj = obj.offsetParent);
-
-		}						
-
-
-		//position autocomplete
-		ac.style.top=pos.pageY - curtop + 40 + "px";
-		ac.style.left=pos.pageX - curleft + 20 + "px";
-		ac.style.display='block';
-		ac.style.background='white';
-
-
-		//remove all options, starting a fresh list
-		ac.options.length = 0;
-
-		//loop through tags and check for a match
-		var tag;
-		for(i in html_tags) {
-			if(!html_tags.hasOwnProperty(i) ){
-				continue;
-			}
-
-			tag=html_tags[i];					
-			if( text ){
-				if( text !== tag.substr(0,text.length) ){
-					continue;
-				}
-			}
-
-			var option = document.createElement('option');
-			option.text = tag;
-			option.value = tag;
-
-			try {
-				ac.add(option, null); // standards compliant; doesn't work in IE
-			}
-			catch(ex) {
-				ac.add(option); // IE only
-			}
-
-		}//end for
-
-
-
-		//if the return list contains everything then don't display it
-		if (html_tags.length === ac.options.length){
-			ac.options.length = 0;
-		}
-
-		//check for matches
-		if ( ac.length === 0 ) {
-			ac.style.display='none';
-			autocompleting=false;
-		} else {
-			ac.selectedIndex=0;			
-			autocompleting=true;
-		}
-
-	});//end editor change event
+	editor.getSession().on('change', onSessionChange);//end editor change event
 
 
 

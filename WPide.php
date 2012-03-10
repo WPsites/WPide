193252208c95a3255d9102fbd1714a95cc20e1cb
Simon D <simon@wpsites.co.uk>
2012-03-10 19:18:11 +0000

    Rather than directly access the file system we are now using the WP file system API

--- WPide.php
+++ WPide.php
@@ -24,6 +24,9 @@ class WPide2
 		//only include this plugin if on theme editor, plugin editor or an ajax call
 		if ( $_SERVER['PHP_SELF'] === '/wp-admin/admin-ajax.php' ||
 			$_GET['page'] === 'wpide' ){
+                
+            //force local file method for testing - you could force other methods 'direct', 'ssh', 'ftpext' or 'ftpsockets'
+            define('FS_METHOD', 'direct'); 
 
 			// Uncomment any of these calls to add the functionality that you need.
 			add_action('admin_head', 'WPide2::add_admin_head');
@@ -230,23 +233,31 @@ class WPide2
         if ( !current_user_can('edit_themes') )
 		wp_die('<p>'.__('You do not have sufficient permissions to edit templates for this site. SORRY').'</p>');
         
+        //setup wp_filesystem api
+        global $wp_filesystem;
+        if ( ! WP_Filesystem($creds) ) 
+            return false;
+        
 		$_POST['dir'] = urldecode($_POST['dir']);
 		$root = WP_CONTENT_DIR;
 		
-		if( file_exists($root . $_POST['dir']) ) {
-			$files = scandir($root . $_POST['dir']);
-			natcasesort($files);
+		if( $wp_filesystem->exists($root . $_POST['dir']) ) {
+			//$files = scandir($root . $_POST['dir']);
+            //print_r($files);
+            $files = $wp_filesystem->dirlist($root . $_POST['dir']);
+            //print_r($files);
+            
 			if( count($files) > 2 ) { /* The 2 accounts for . and .. */
 				echo "<ul class=\"jqueryFileTree\" style=\"display: none;\">";
 				// All dirs
-				foreach( $files as $file ) {
-					if( file_exists($root . $_POST['dir'] . $file) && $file != '.' && $file != '..' && is_dir($root . $_POST['dir'] . $file) ) {
+				foreach( $files as $file => $file_info ) {
+					if( $file != '.' && $file != '..' && $file_info['type']=='d' ) {
 						echo "<li class=\"directory collapsed\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "/\">" . htmlentities($file) . "</a></li>";
 					}
 				}
 				// All files
-				foreach( $files as $file ) {
-					if( file_exists($root . $_POST['dir'] . $file) && $file != '.' && $file != '..' && !is_dir($root . $_POST['dir'] . $file) ) {
+				foreach( $files as $file => $file_info ) {
+					if( $file != '.' && $file != '..' &&  $file_info['type']!='d') {
 						$ext = preg_replace('/^.*\./', '', $file);
 						echo "<li class=\"file ext_$ext\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "\">" . htmlentities($file) . "</a></li>";
 					}
@@ -264,10 +275,16 @@ class WPide2
         check_admin_referer('plugin-name-action_wpidenonce'); 
         if ( !current_user_can('edit_themes') )
 		wp_die('<p>'.__('You do not have sufficient permissions to edit templates for this site. SORRY').'</p>');
+        
+        //setup wp_filesystem api
+        global $wp_filesystem;
+        if ( ! WP_Filesystem($creds) ) 
+            return false;
+        
          
 		$root = WP_CONTENT_DIR;
 		$file_name = $root . stripslashes($_POST['filename']);
-		echo file_get_contents($file_name);
+		echo $wp_filesystem->get_contents($file_name);
 		die(); // this is required to return a proper result
 	}
 	
@@ -277,23 +294,26 @@ class WPide2
         if ( !current_user_can('edit_themes') )
 		wp_die('<p>'.__('You do not have sufficient permissions to edit templates for this site. SORRY').'</p>');
         
+        //setup wp_filesystem api
+        global $wp_filesystem;
+        if ( ! WP_Filesystem($creds) ) 
+            echo "Cannot initialise the WP file system API";
+        
         //save a copy of the file and create a backup just in case
 		$root = WP_CONTENT_DIR;
 		$file_name = $root . stripslashes($_POST['filename']);
-		
-		//todo: need to check user is able to edit this file
 		
 		//set backup filename
 		$backup_path =  ABSPATH .'wp-content/plugins/' . basename(dirname(__FILE__)) .'/backups/' . str_replace( str_replace('\\', "/", ABSPATH), '', $file_name) .'.'.date("YmdH");
 		//create backup directory if not there
 		$new_file_info = pathinfo($backup_path);
-		if (!is_dir($new_file_info['dirname'])) mkdir($new_file_info['dirname'], 0777, true);
+		if (!$wp_filesystem->is_dir($new_file_info['dirname'])) $wp_filesystem->mkdir($new_file_info['dirname'], 0775);
 		
         //do backup
-		file_put_contents($backup_path,  file_get_contents($file_name) );
-		
+		$wp_filesystem->copy( $file_name, $backup_path );
+        
         //save file
-		if( file_put_contents($file_name, stripslashes($_POST['content'])  ) ) echo "success";
+		if( $wp_filesystem->put_contents( $file_name, stripslashes($_POST['content'])) ) echo "success";
 		die(); // this is required to return a proper result
 	}
 	
@@ -320,7 +340,13 @@ class WPide2
 		});
 		</script>
 		
-		
+<?php
+$url = wp_nonce_url('admin.php?page=wpide','plugin-name-action_wpidenonce');
+if ( ! WP_Filesystem($creds) ) {
+    request_filesystem_credentials($url, '', true, false, null);
+	return;
+}
+?>
 			
 <div id="wpide_toolbar" class="quicktags-toolbar"> 
   <div id="wpide_toolbar_tabs"> </div>
